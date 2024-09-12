0f9ac253ac1f7caff0a12c38bc636418b70bbc67
Rifat Hossain <mdrifathossain472@gmail.com>
2024-09-12 22:45:46 +0600

    mvvm added

---
+++ app/src/main/java/com/esports/bazar/view/login/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.esports.bazar.view.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.esports.bazar.R
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
